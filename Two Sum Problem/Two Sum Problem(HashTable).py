c32f3b677de60ceae8410ab06d8e2836112380dc
REZA DYSTA SATRIA <87264688+dyzzta@users.noreply.github.com>
2023-02-20 22:10:17 +0300

    Update Two Sum Problem(HashTable).py

--- Two Sum Problem/Two Sum Problem(HashTable).py
+++ Two Sum Problem/Two Sum Problem(HashTable).py
@@ -4,6 +4,4 @@ class Solution(object):
         for i in range(len(nums)):
             if target - nums[i] in hashtable:
                 return [hashtable[target-nums[i]],i]
-            hashtable[nums[i]] = i # Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
-print("Hello world")
+            hashtable[nums[i]] = i 
