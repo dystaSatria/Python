132ea40077d3f7d44e9003c2ce20c3babfae0320
REZA DYSTA SATRIA <87264688+dyzzta@users.noreply.github.com>
2023-02-24 21:20:17 +0300

    Daily Python by dyzzta

---
+++ ReverseNumber/StringFunction.py
@@ -0,0 +1,10 @@
+# this is an easist way 
+
+def reverseNum(num):
+  return int(str(num)[::-1])
+
+
+
+while True :
+    num = input("\ninput your number\n")
+    print (f"The reverse number is {reverseNum(num)}")
