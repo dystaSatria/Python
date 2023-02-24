667889a249977b8a4e789ac4b5a8848cb67cc8b9
REZA DYSTA SATRIA <87264688+dyzzta@users.noreply.github.com>
2023-02-24 23:33:36 +0300

    Update withLoop.py

--- ReverseNumber/withLoop.py
+++ ReverseNumber/withLoop.py
@@ -1,9 +1,10 @@
-
 Num = int(input("input the number"))
 operation = 0
 
-While Num != 0 :
+While Num != 0:
    reverseNum = Num % 10
    operation = (operation* Num) + reverseNum
-   Num 
-
+   Num // 10
+   
+print (str(reverseNum))
+   
