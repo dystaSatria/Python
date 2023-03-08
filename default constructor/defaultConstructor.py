82043d182a26b858a8e7d2733b9ccfb51beff3d1
REZA DYSTA SATRIA <87264688+dyzzta@users.noreply.github.com>
2023-03-09 00:27:50 +0300

    Create defaultConstructor.py

---
+++ default constructor/defaultConstructor.py
@@ -0,0 +1,38 @@
+
+#__init__
+class Person:
+    def __init__(abe,nama,aga):
+        abe.firstnama = nama
+        abe.lastaga = aga
+        
+    def myFun(abcd):
+        print("Hei i am "+ abcd.firstnama )
+
+    
+# p1 = Person ("BB", 12)
+
+
+
+# print (p1.firstnama)
+# p1.aga = "100"
+
+# print(p1.lastaga)
+# p1.myFun()
+
+# print("**************")
+
+# class Hoca(Person):
+#     def __init__(abe, nama, aga, reis):
+#         super().__init__(nama, aga)
+#         abe.graduation = reis
+    
+#     def A(agd):
+#         print("Welcome", agd.firstnama, agd.lastaga, agd.graduation)
+
+
+# p2 = Hoca("Ahmed",90,46)
+
+# p3 = Person("Ali",20)
+# p3.myFun()
+# p2.A()
+
