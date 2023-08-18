b2561a76ad833f935d0e6dc6ba59993ca92aaa49
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-08-18 14:56:35 +0300

    Update main.py

--- Capitalize word Manchine /main.py
+++ Capitalize word Manchine /main.py
@@ -1,7 +1,7 @@
 def capitalize(x):
         return x.upper()
 
-while 1 :
+while True :
     print("=========== Capitalize word Manchine =========")
     inputUser = input("Input : ")         
     print(capitalize(str(inputUser) + "\n"))
