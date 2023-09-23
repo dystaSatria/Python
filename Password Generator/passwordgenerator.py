cbdedc851b43693df89a44e228ba0e4107f082fc
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-09-23 16:11:44 -0400

    Update passwordgenerator.py

--- Password Generator/passwordgenerator.py
+++ Password Generator/passwordgenerator.py
@@ -1,1 +1,16 @@
 
+import random
+import string
+
+def generate_password(length):
+    
+    characters = string.ascii_letters + string.digits
+    
+    
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+length = int(input("Masukkan panjang kata sandi yang diinginkan: "))
+
+password = generate_password(length)
+print("Kata sandi acak Anda adalah:", password)
