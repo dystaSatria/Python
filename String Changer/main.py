efa1fbe37cb39b2265b13b21a082c2f1f3d3d409
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-10-05 15:35:37 -0400

    Update main.py

--- String Changer/main.py
+++ String Changer/main.py
@@ -1,1 +1,14 @@
+print("=== Changer Symbol String ===")
+print("Example for changing '-' symbol to 'to' character \n\n")
+
+while True:
+    targetString = input("convert '-' -> 'to' (or enter 'quit' to exit): ")
+
+    if targetString == 'quit':
+        break
+
+    updatedString = targetString.replace("-", "to")
+
+    print(f"Modified string: {updatedString}\n")
+    print("============================")
 
