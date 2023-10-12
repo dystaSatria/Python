e723001e2d720aa4eb2d02269b7a53e247150a0a
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-10-12 16:23:13 -0400

    Update main.py

--- Calculator Tkinter/main.py
+++ Calculator Tkinter/main.py
@@ -63,6 +63,7 @@ btn_multiple = tk.Button(root, text="x", command=lambda: add_to_calculation(*), 
 btn_muitiple.grid(row=4, column=4)
 btn_div = tk.Button(root, text=":", command=lambda: add_to_calculation(/), width=5, font=("Arial",14))
 btn_div.grid(row=5, column=4)
-
+btn_open = tk.Button(root, text="(", command=lambda: add_to_calculation("("), width=5, font=("Arial",14))
+btn_open.grid(row=5, column=4)
 
 root.mainLoop()
